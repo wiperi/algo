29e03246f5b6805c4d2b7423badcea60a63638cb
wiperi <boris1151@163.com>
2023-07-11 01:58:28 +0800

    up

--- Algorithm Part I by Princeton University/HelloWorld.java
+++ Algorithm Part I by Princeton University/HelloWorld.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static void staticmethod() {
+        System.out.println("Fuck you, World!");
+    }
+
 }
