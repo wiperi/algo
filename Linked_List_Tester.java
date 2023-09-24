2ed3e8e10d726e884901697a5e57bc834ba19938
Magician <solidtreepassing@gmail.com>
2023-09-25 00:27:32 +0800

    up

---
+++ Linked_List_Tester.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Linked_List_Tester {
+
+    ListNode head;
+
+    private static class ListNode {
+        ListNode() {
+            this.val = 0;
+            this.next = null;
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        int val;
+        ListNode next;
+    }
+
+    void add(ListNode head) {
+        ListNode newhead = new ListNode();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
