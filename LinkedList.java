421f4b064ae7c366d14a19d6bec87b37fcf19fc5
wiperi <solidtreepassing@gmail.com>
2023-10-05 02:21:33 +0800

    up

---
+++ LinkedList.java
@@ -0,0 +1,78 @@
+import Chap1_Fundamental.Section3_Bag_Queue_Stack.Node;
+import edu.princeton.cs.algs4.Queue;
+
+public class LinkedList<Item> {
+    @SuppressWarnings("unchecked")
+    public Node<Item>[] arr = (Node<Item>[]) new Object[100];
+    public Queue<Node<Item>> aee = new Queue<>();
+    public Node<Item> head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    /**
+     * 当且仅当泛型参数为{@code String}的时候可以使用此构造函数，该函数接受一个字符串，读取其中每一个由空格分隔的元素，生成一条链表
+     * 
+     * @param s 用于构建链表的字符串
+     */
+    @SuppressWarnings("unchecked")
+    public LinkedList(String s) {
+        Item[] tokens = (Item[]) s.split("\\s+");
+        for (int i = 0; i < tokens.length; i++) {
+            addBackNode(tokens[i]);
+        }
+    }
+
+    public Node<Item> getHead() {
+        aee.
+        return head;
+    }
+
+    public void addFrontNode(Item item) {
+        Node<Item> newNode = new Node<Item>(item, head);
+        head = newNode;
+    }
+
+    public void addBackNode(Item item) {
+        Node<Item> newNode = new Node<Item>(item, null);
+        arr[10] = newNode;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<Item> cur = head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+    }
+
+    public void print() {
+        Node<Item> cur = head;
+        System.out.print("-> ");
+        while (cur != null) {
+            System.out.print(cur.item + " -> ");
+            cur = cur.next;
+        }
+        System.out.print(cur + "\n");
+    }
+
+    @Override
+    public String toString() {
+        Node<Item> cur = head;
+        String s = "-> ";
+        while (cur != null) {
+            s += cur.item.toString() + " -> ";
+            cur = cur.next;
+        }
+        s += cur;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> l = new LinkedList<>("1 2 3 4 5");
+        l.print();
+        System.out.println(l);
+    }
+}
--- man.java
+++ man.java
@@ -1,37 +1,9 @@
-import java.util.HashSet;
-import java.util.Set;
-
-import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.Date;
-
+/**
+ * man
+ */
 public class man {
+
     public static void main(String[] args) {
-        try {
-            int result = divide(10, 0);
-            // 可能引发异常的代码
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            // 捕获并处理ArithmeticException异常
-            System.out.println("An arithmetic exception occurred: " + e.getMessage());
-        } finally {
-            // 无论是否发生异常，这里的代码都会执行
-            System.out.println("Finally block executed.");
-
-            Date date = new Date(12, 12, 1);
-            Bag<Integer> bag = new Bag<Integer>();
-            
-            Set<String> a = new HashSet<>(100);
-            
-
-
-
-
-
-        }
-    }
-
-    public static int divide(int num1, int num2) {
-        // 尝试除法操作，可能引发ArithmeticException异常
-        return num1 / num2;
+        
     }
 }
