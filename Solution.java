f2aba18368d6147c3c2f50b861c5a86c571e9c51
wiperi <solidtreepassing@gmail.com>
2023-10-16 07:55:43 +0800

    up

--- Solution.java
+++ Solution.java
@@ -5,94 +5,30 @@ import java.util.HashMap;
  */
 public class Solution {
 
-    public static boolean checkInclusion(String s1, String s2) {
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-        if (s2Len < s1Len)
-            return false;
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        int len = nums.length;
 
-        HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
+        HashMap<Integer, Integer> window = new HashMap<>();
 
-        for (Character character : s1.toCharArray()) {
-            need.put(character, need.getOrDefault(character, 0) + 1);
+        for (int i = 0; i < k; i++) {
+            window.put(nums[i], window.getOrDefault(nums[i], 0) + 1);
         }
-        System.out.println(need); // debug
-
-        int left = 0, right = 0;
-        int valid = 0;
-        while (right < s2Len) {
-            char Rch = s2.charAt(right);
-            right++;
-            if (need.containsKey(Rch)) { // extend right side
-                window.put(Rch, window.getOrDefault(Rch, 0) + 1);
-                System.out.println("window updated: " + window); // debug
-                if (window.get(Rch).equals(need.get(Rch))) {
-                    valid++;
-                }
-            }
-
-            int len = right - left;
-            while (len >= s1Len) {
-                if (valid == need.size()) {
-                    return true;
-                }
-                char Lch = s2.charAt(left);
-                left++;
-                len = right - left;
-                if (need.containsKey(Lch)) { // shrink left side
-                    if (window.get(Lch).equals(need.get(Lch))) {
-                        valid--;
-                    }
-                    window.put(Lch, window.getOrDefault(Lch, 0) - 1);
-                    System.out.println("window shrinked: " + window); // debug
-                }
-            }
-
+        for (int i : window.values()) {
+            if (i > 1)
+                return true;
         }
-        return false;
-    }
 
-    public static boolean checkInclusion1(String t, String s) {
-        HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            need.put(c, need.getOrDefault(c, 0) + 1);
-        }
-    
-        int left = 0, right = 0;
-        int valid = 0;
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            right++;
-            if (need.containsKey(c)) {
-                window.put(c, window.getOrDefault(c, 0) + 1);
-                if (window.get(c).equals(need.get(c)))
-                    valid++;
-            }
-    
-            // 判断左侧窗口是否要收缩
-            while (right - left >= t.length()) {
-                // 在这里判断是否找到了合法的子串
-                if (valid == need.size())
-                    return true;
-                char d = s.charAt(left);
-                left++;
-                if (need.containsKey(d)) {
-                    if (window.get(d).equals(need.get(d)))
-                        valid--;
-                    window.put(d, window.getOrDefault(d, 0) - 1);
-                }
+        for (int i = k; i < len; i++) {
+            window.put(nums[i], window.getOrDefault(nums[i], 0) + 1);
+            window.put(nums[i - k], window.getOrDefault(nums[i], 0) - 1);
+            if (window.get(nums[i]) > 1) {
+                return true;
             }
         }
-        // 未找到符合条件的子串
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkInclusion("ab", "wab"));
-        System.out.println(checkInclusion1("ab", "wab"));
-
+        System.out.println(containsNearbyDuplicate(new int[] { 1, 2, 3, 4 }, 3));
     }
 }
