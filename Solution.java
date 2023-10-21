c522512ea12d7cac550d96983ea4fe99abfc643c
wiperi <solidtreepassing@gmail.com>
2023-10-21 23:15:10 +0800

    up

--- Solution.java
+++ Solution.java
@@ -1,11 +1,20 @@
-import edu.princeton.cs.algs4.Quick;
-
 /**
  * Solution
  */
 public class Solution {
 
-    public static void main(String[] args) {
-        
+    public int subarraySum(int[] nums, int n) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+                if (sum == n)
+                    count++;
+            }
+        }
+        return count;
     }
 }
