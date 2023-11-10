a6caa256a5cd722bf43f99594a6702aa3c6b6aa7
wiperi <solidtreepassing@gmail.com>
2023-11-11 00:24:15 +0800

    up

--- Chap3_Search/_BST.java
+++ Chap3_Search/_BST.java
@@ -1,12 +1,15 @@
 package Chap3_Search;
 
+import java.util.Arrays;
+import java.util.List;
+
 import edu.princeton.cs.algs4.BST;
 
 public class _BST {
     public TreeNode root;
 
     /************************************************************************
-     * Core Functions
+     * Object Methods
      ************************************************************************/
     public TreeNode search(int val) {
         TreeNode cur = root;
@@ -26,7 +29,7 @@ public class _BST {
             root = new TreeNode(val);
             return;
         }
-
+        // find the right position to attach
         TreeNode pre = null, cur = root;
         while (cur != null) {
             if (val == cur.val) {
@@ -39,13 +42,100 @@ public class _BST {
                 cur = cur.left;
             }
         }
-
+        // attach new node
         TreeNode newNode = new TreeNode(val);
         if (val > pre.val) {
             pre.right = newNode;
         } else {
             pre.left = newNode;
         }
+    }
+
+    public List<Integer> toSortedArray() {
+        return TreeNode.dfs(root, 2);
+    }
+
+    /************************************************************************
+     * Class Methods
+     ************************************************************************/
+    public static boolean isValidBST(TreeNode root) {
+        isValidBSTAux(root);
+        boolean res = valid;
+        // reset value
+        valid = true;
+        preVal = null;
+
+        return res;
+    }
+
+    private static boolean valid = true;
+    private static Integer preVal;
+
+    private static void isValidBSTAux(TreeNode root) {
+        if (root == null)
+            return;
+        isValidBSTAux(root.left);
+        // the inorder traverse of a BST must be a ascending ordered list
+        if (preVal == null)
+            preVal = root.val;
+        else {
+            if (root.val <= preVal) {
+                valid = false;
+                return;
+            }
+            preVal = root.val;
+        }
+        isValidBSTAux(root.right);
+    }
+
+    public boolean isValidBSTVerLabuladong(TreeNode root) {
+        return isValidBSTVerLabuladongAux(root, null, null);
+    }
+
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    boolean isValidBSTVerLabuladongAux(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null)
+            return true;
+        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+        if (min != null && root.val <= min.val)
+            return false;
+        if (max != null && root.val >= max.val)
+            return false;
+        // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+        return isValidBSTVerLabuladongAux(root.left, min, root)
+                && isValidBSTVerLabuladongAux(root.right, root, max);
+    }
+
+    public static TreeNode buildFromSortedArray(int[] a) {
+        return buildFromSortedArrayAux(null, a, 0, a.length - 1);
+    }
+
+    private static TreeNode buildFromSortedArrayAux(TreeNode root, int[] arr, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (lo <= hi) {
+            root = new TreeNode(arr[mid]);
+        } else {
+            return null;
+        }
+
+        root.left = buildFromSortedArrayAux(root.left, arr, lo, mid - 1);
+        root.right = buildFromSortedArrayAux(root.right, arr, mid + 1, hi);
+        return root;
+    }
+
+    private static boolean isSorted(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i - 1])
+                return false;
+        }
+        return true;
+    }
+
+    public static TreeNode buildFromArray(int[] a) {
+        if (!isSorted(a))
+            Arrays.sort(a);
+        return buildFromSortedArray(a);
     }
 
     /************************************************************************
@@ -77,5 +167,11 @@ public class _BST {
         System.out.println(TreeNode.preorderIter1(b.root));
         System.out.println(TreeNode.inorderIter(b.root));
         System.out.println(TreeNode.postorderIter(b.root));
+
+        System.out.println();
+
+        System.out.println("toSortedArray");
+        List<Integer> inorderList = b.toSortedArray();
+        System.out.println(inorderList);
     }
 }
--- Solution.java
+++ Solution.java
@@ -4,173 +4,85 @@ import java.util.List;
 import java.util.Queue;
 
 import Chap3_Search.TreeNode;
+import Chap3_Search._BST;
 
 public class Solution {
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        maxDepth(root);
-        return res;
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(null, nums, 0, nums.length - 1);
     }
 
-    int res = 0;
-
-    public int maxDepth(TreeNode root) {
-        // 分解问题：最长直径 = 左子树的最长路径 + 右子树的最长路径
-        if (root == null)
-            return 0;
-
-        int leftMax = maxDepth(root.left);
-        int rightMax = maxDepth(root.right);
-
-        res = Math.max(res, leftMax + rightMax);
-        return 1 + Math.max(leftMax, rightMax);
-    }
-
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null)
+    public TreeNode buildBST(TreeNode root, int[] arr, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (lo <= hi) {
+            root = new TreeNode(arr[mid]);
+        } else {
             return null;
-
-        Queue<TreeNode> que = new LinkedList<>();
-        List<TreeNode> list = new LinkedList<>();
-        List<Integer> subList = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        que.offer(root);
-
-        while (true) {
-            for (TreeNode node : que) {
-                list.add(node);
-            }
-
-            for (TreeNode node : list) {
-                int a = node.val;
-                System.out.println(a);
-                subList.add(a);
-            }
-            res.add(subList);
-            subList.clear();
-
-            que.clear();
-            for (TreeNode node : list) {
-                if (node.left != null)
-                    que.offer(node.left);
-                if (node.right != null)
-                    que.offer(node.right);
-            }
-            if (que.isEmpty())
-                break;
-            list.clear();
         }
-        return res;
+
+        root.left = buildBST(root.left, arr, lo, mid - 1);
+        root.right = buildBST(root.right, arr, mid + 1, hi);
+        return root;
     }
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        // 方法一：BFS
-        // if(root == null){
-        //     return new ArrayList<>();
-        // }
-        // List<List<Integer>> res = new ArrayList<>();
-        
-
-        // LinkedList<TreeNode> queue = new LinkedList<>();
-        // queue.add(root);
-
-        // while(!queue.isEmpty()){
-        //     int size = queue.size();
-        //     List<Integer> tempList = new ArrayList<>();
-
-        //     while(size > 0){
-        //         TreeNode temp = queue.poll();
-        //         tempList.add(temp.val);
-
-        //         if(temp.left != null){
-        //             queue.add(temp.left);
-        //         }
-
-        //         if(temp.right != null){
-        //             queue.add(temp.right);
-        //         }
-        //         size--;
-
-        //     }
-
-        //     res.add(tempList);
-
-        // }
-
-        // return res;
-
-
-        // 方法二： DFS
-        DFS(root, 0);
-        return res;
-
-
+    public boolean wwwwwwwwwwwwwisValidBST(TreeNode root) {
+        boolean valid = false;
+        if (root == null)
+            return true;
+        else {
+            if (root.right == null && root.left == null)
+                valid = true;
+            if (root.right != null && root.left != null) {
+                if (root.right.val > root.val && root.val > root.left.val)
+                    valid = true;
+            }
+            if (root.right == null && root.left != null) {
+                if (root.val > root.left.val)
+                    valid = true;
+            }
+            if (root.right != null && root.left == null) {
+                if (root.right.val > root.val)
+                    valid = true;
+            }
+        }
+        System.out.println(root.val + " is " + valid);
+        return valid && wwwwwwwwwwwwwisValidBST(root.left) && wwwwwwwwwwwwwisValidBST(root.right);
     }
-    List<List<Integer>> res = new ArrayList<>();
 
-    public void DFS(TreeNode root,int  level){
-        if(root == null){
+    public boolean isValidBST(TreeNode root) {
+        dfs(root);
+        return valid;
+    }
+
+    static boolean valid = true;
+    static Integer val;
+
+    private void dfs(TreeNode root) {
+        if (root == null)
             return;
-        }
-        
-        if(res.size() == level){
-            res.add(new ArrayList<Integer>());
-        }
+        dfs(root.left);
 
-        res.get(level).add(root.val);
-
-        DFS(root.left, level + 1);
-        DFS(root.right, level + 1);
-
-    }
-}
-
-    public List<List<Integer>> levelOrderAns(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
-        if (root == null) {
-            return res;
-        }
-
-        Queue<TreeNode> que = new LinkedList<>();
-        que.offer(root);
-        // while 循环控制从上向下一层层遍历
-        while (!que.isEmpty()) {
-            int sz = que.size();
-            // 记录这一层的节点值
-            List<Integer> level = new LinkedList<>();
-            // for 循环控制每一层从左向右遍历
-            for (int i = 0; i < sz; i++) {
-                TreeNode cur = que.poll();
-                level.add(cur.val);
-                if (cur.left != null)
-                    que.offer(cur.left);
-                if (cur.right != null)
-                    que.offer(cur.right);
+        // inorder position
+        if (val == null)
+            val = root.val;
+        else {
+            if (root.val <= val) {
+                valid = false;
+                return;
             }
-            res.add(level);
+            val = root.val;
         }
-        return res;
+
+        dfs(root.right);
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        TreeNode root = TreeNode.builtTree(new int[] { 1, 2, 3, 4, 5 });
-        System.out.println(s.levelOrder(root));
+
+        TreeNode root = TreeNode.builtTree(new int[] { 5, 4, 6, 3, 7 });
+        System.out.println(s.wwwwwwwwwwwwwisValidBST(root));
+
+        _BST b = new _BST();
+        _BST.isValidBST(root);
     }
 }
