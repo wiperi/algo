1ea6772e0563ad935af6dd30cf1abd68c913b6d5
wiperi <solidtreepassing@gmail.com>
2023-11-19 00:45:32 +0800

    Refactor merge sort implementation

--- Week3_Elementary_Sort/_12_MergeSortCuiVer.java
+++ Week3_Elementary_Sort/_12_MergeSortCuiVer.java
@@ -1,14 +1,10 @@
 package Week3_Elementary_Sort;
 
-import static org.junit.Assert.assertArrayEquals;
-
-import org.junit.jupiter.api.Assertions;
-
 public class _12_MergeSortCuiVer {
     public int[] sortArray(int[] nums) {
         if (nums.length == 1) return nums;
         int[][] sub = split(nums);
-        
+
         return merge(sortArray(sub[0]), sortArray(sub[1]));
     }
 
@@ -44,56 +40,5 @@ public class _12_MergeSortCuiVer {
             while (i < left.length) ret[cur++] = left[i++];
         }
         return ret;
-    }
-
-    public static void main(String[] args) {
-        _12_MergeSortCuiVer s = new _12_MergeSortCuiVer();
-        s.testMerge();
-        s.testSplit();
-
-        System.out.println();
-
-        s.testSortArray();
-    }
-
-    public static void show(int[] a) {
-        for (int i : a) {
-            System.out.print(i);
-        }
-        System.out.println();
-    }
-
-    protected void testMerge() {
-        int[] left = { 1, 3, 5 };
-        int[] right = { 2, 4, 6 };
-        int[] merged = merge(left, right);
-        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, merged);
-    }
-
-    protected void testSplit() {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
-        int[][] subArrays = split(arr);
-
-        System.out.println("Subarray 1:");
-        show(subArrays[0]);
-
-        System.out.println("Subarray 2:");
-        show(subArrays[1]);
-    }
-
-    public void testSortArray() {
-        _12_MergeSortCuiVer sorter = new _12_MergeSortCuiVer();
-
-        int[] nums = { 5, 2, 8, 3, 1 };
-        int[] sorted = sorter.sortArray(nums);
-        System.out.println("sorted:");
-        show(sorted);
-        Assertions.assertArrayEquals(new int[] { 1, 2, 3, 5, 8 }, sorted);
-
-        int[] nums2 = { 10, 7, 3, 9, 6 };
-        int[] sorted2 = sorter.sortArray(nums2);
-        Assertions.assertArrayEquals(new int[] { 3, 6, 7, 9, 10 }, sorted2);
-
-        // Add more test cases as needed
     }
 }
