6c15ed58b999c75cc3a46ce243775bd2c5688f40
Magician <solidtreepassing@gmail.com>
2023-12-23 01:12:03 +0800

    Updated .gitignore file to ignore JSON files and binaries

---
+++ cpptest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello\n";
+    return 0;
+}
+
---
+++ multi_thread.java
@@ -0,0 +1,96 @@
+import java.util.Arrays;
+
+public class multi_thread {
+    public static void use_threads(String[] args) {
+        Thread th1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("th1 runs " + i);
+                }
+            }
+        });
+
+        Thread th2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("th2 runs " + i);
+                }
+            }
+        });
+
+        Thread th3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("th3 runs " + i);
+                }
+            }
+        });
+
+        th3.start();
+        th1.start(); // Start thread 1
+        th2.start(); // Start thread 2
+    }
+
+    class SleepSort {
+        public static void main(String[] args) {
+            int[] arr = { 5, 3, 1, 4, 2 };
+            int[] sortedArr = new int[arr.length];
+
+            Thread[] threads = new Thread[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                final int index = i;
+                threads[i] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(arr[index] * 100); // Sleep for number seconds
+                            sortedArr[index] = arr[index];
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Arrays.sort(sortedArr);
+
+            System.out.println("Sorted numbers: " + Arrays.toString(sortedArr));
+        }
+
+        static public void sort(int[] arr) {
+            Thread[] threads = new Thread[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                final int index = i;
+                threads[i] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(arr[index] * 1);
+                            System.out.println(arr[index] + " ");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                threads[i].start();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SleepSort.sort(new int[] { 1, 1000, 2, 3, 4, 5 });
+    }
+}
