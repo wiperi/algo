7d9024877b88f297c72bf590fa5764a673235925
wiperi <solidtreepassing@gmail.com>
2023-12-25 16:51:20 +0800

    refactoring

--- Chap3_Search/Binary_Tree.java
+++ Chap3_Search/Binary_Tree.java
@@ -8,35 +8,34 @@ import java.util.Stack;
 
 import edu.princeton.cs.algs4.BST;
 
-/**
- * Nodes of binary tree
- */
-public class TreeNode {
-    public Integer val;
-    public TreeNode left;
-    public TreeNode right;
+public class Binary_Tree {
+    public static class Node {
+        public Integer val;
+        public Node left;
+        public Node right;
 
-    public TreeNode() {}
+        public Node() {}
 
-    public TreeNode(Integer val) {
-        this.val = val;
-    }
+        public Node(Integer val) {
+            this.val = val;
+        }
 
-    public TreeNode(Integer val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        public Node(Integer val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 
     /************************************************************************
      * Helper Funtions
      ************************************************************************/
-    public static int sizeOf(TreeNode root) {
+    public static int sizeOf(Node root) {
         List<Integer> list = levelOrder(root);
         return list.size();
     }
 
-    public static int getHeight(TreeNode root) {
+    public static int getHeight(Node root) {
         if (root == null) return 0;
 
         int leftMax = getHeight(root.left);
@@ -45,11 +44,11 @@ public class TreeNode {
         return 1 + Math.max(leftMax, rightMax);
     }
 
-    public static TreeNode buildTree(Integer[] a) {
+    public static Node buildTree(Integer[] a) {
         int len = a.length;
-        TreeNode[] nodes = new TreeNode[len];
+        Node[] nodes = new Node[len];
         for (int i = 0; i < len; i++) {
-            nodes[i] = new TreeNode(a[i], null, null);
+            nodes[i] = new Node(a[i], null, null);
         }
         for (int i = 0; i < len; i++) {
             int leftKid = 2 * i + 1;
@@ -65,7 +64,7 @@ public class TreeNode {
      * 
      * @param root
      */
-    public static void show(TreeNode root) {
+    public static void show(Node root) {
         int arrSize = (int) Math.pow(2, (double) getHeight(root)) - 1;
         Integer[] arr = new Integer[arrSize];
         showAux(root, arr, 0);
@@ -90,7 +89,7 @@ public class TreeNode {
         }
     }
 
-    private static void showAux(TreeNode root, Integer[] arr, int index) {
+    private static void showAux(Node root, Integer[] arr, int index) {
         if (root == null) {
             return;
         }
@@ -109,18 +108,18 @@ public class TreeNode {
      * BFS
      ************************************************************************/
     // 层序遍历
-    public static List<Integer> levelOrder(TreeNode root) {
+    public static List<Integer> levelOrder(Node root) {
         // video: https://youtu.be/my9AYTFl8IM
         if (root == null) {
             return null;
         }
 
-        Queue<TreeNode> que = new LinkedList<>();
+        Queue<Node> que = new LinkedList<>();
         List<Integer> ret = new ArrayList<>();
 
         que.add(root);
         while (!que.isEmpty()) {
-            TreeNode temp = que.poll(); // 取出节点
+            Node temp = que.poll(); // 取出节点
             ret.add(temp.val); // 记录节点
 
             if (temp.left != null) // 左节点入队
@@ -131,13 +130,13 @@ public class TreeNode {
         return ret;
     }
 
-    public static List<List<Integer>> levelOrderReturnLists(TreeNode root) {
+    public static List<List<Integer>> levelOrderReturnLists(Node root) {
         List<List<Integer>> res = new LinkedList<>();
         levelOrderReturnListsAux(root, 1, res);
         return res;
     }
 
-    private static void levelOrderReturnListsAux(TreeNode root, int level, List<List<Integer>> res) {
+    private static void levelOrderReturnListsAux(Node root, int level, List<List<Integer>> res) {
         if (root == null) {
             return;
         }
@@ -149,18 +148,18 @@ public class TreeNode {
         levelOrderReturnListsAux(root.right, level + 1, res);
     }
 
-    public static List<List<Integer>> levelOrderReturnListsIter(TreeNode root) {
+    public static List<List<Integer>> levelOrderReturnListsIter(Node root) {
         if (root == null) return new ArrayList<>();
 
         List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> que = new LinkedList<>();
+        Queue<Node> que = new LinkedList<>();
         que.offer(root);
         while (!que.isEmpty()) {
             int levelSize = que.size();
 
             List<Integer> level = new ArrayList<>();
             for (int i = 0; i < levelSize; i++) {
-                TreeNode cur = que.poll();
+                Node cur = que.poll();
                 level.add(cur.val);
                 if (cur.left != null) que.offer(cur.left);
                 if (cur.right != null) que.offer(cur.right);
@@ -174,7 +173,7 @@ public class TreeNode {
      * DFS
      ************************************************************************/
     @SuppressWarnings("unused")
-    private static void dfsFrame(TreeNode root) {
+    private static void dfsFrame(Node root) {
         if (root == null) {
             return;
         }
@@ -194,12 +193,12 @@ public class TreeNode {
      * @param mode
      * @return
      */
-    public static List<Integer> dfs(TreeNode root, int mode) {
+    public static List<Integer> dfs(Node root, int mode) {
         retList.clear();
         return dfsAux(root, mode);
     }
 
-    private static List<Integer> dfsAux(TreeNode root, int mode) {
+    private static List<Integer> dfsAux(Node root, int mode) {
         if (mode < 1 || mode > 3) { // 检查参数合法性
             throw new IllegalArgumentException("mode must be 1,2 or 3");
         }
@@ -221,7 +220,7 @@ public class TreeNode {
         return retList;
     }
 
-    public static List<Integer> preorderSub(TreeNode root) {
+    public static List<Integer> preorderSub(Node root) {
         // 分解成问题：一个二叉树的前序遍历结果 = root的值 + 左子树的前序结果 + + 右子树的前序结果
         List<Integer> ret = new ArrayList<>();
 
@@ -237,16 +236,16 @@ public class TreeNode {
         return ret;
     }
 
-    public static List<Integer> preorderIter(TreeNode root) { // 迭代先序
+    public static List<Integer> preorderIter(Node root) { // 迭代先序
         if (root == null) {
             return null;
         }
-        Stack<TreeNode> stack = new Stack<>();
+        Stack<Node> stack = new Stack<>();
         List<Integer> ret = new ArrayList<>();
 
         stack.push(root);
         while (!stack.empty()) {
-            TreeNode cur = stack.pop();
+            Node cur = stack.pop();
             ret.add(cur.val); // 记录节点
 
             if (cur.right != null) {
@@ -260,8 +259,8 @@ public class TreeNode {
         return ret;
     }
 
-    public static List<Integer> preorderIter1(TreeNode root) { // 迭代先序1
-        Stack<TreeNode> stack = new Stack<>();
+    public static List<Integer> preorderIter1(Node root) { // 迭代先序1
+        Stack<Node> stack = new Stack<>();
         List<Integer> ret = new ArrayList<>();
 
         while (!stack.empty() || root != null) {
@@ -278,9 +277,9 @@ public class TreeNode {
         return ret;
     }
 
-    public static List<Integer> inorderIter(TreeNode root) { // 迭代中序
+    public static List<Integer> inorderIter(Node root) { // 迭代中序
         // video: https://youtu.be/d4KBiARy_Tk
-        Stack<TreeNode> stack = new Stack<>();
+        Stack<Node> stack = new Stack<>();
         List<Integer> ret = new ArrayList<>();
 
         while (!stack.empty() || root != null) {
@@ -297,17 +296,17 @@ public class TreeNode {
         return ret;
     }
 
-    public static List<Integer> postorderIter(TreeNode root) { // 迭代后序
+    public static List<Integer> postorderIter(Node root) { // 迭代后序
         if (root == null) {
             return null;
         }
 
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
         stack1.push(root);
 
         while (!stack1.isEmpty()) {
-            TreeNode current = stack1.pop();
+            Node current = stack1.pop();
             stack2.push(current);
 
             // 先将左子树入stack1，再将右子树入stack1
@@ -331,7 +330,7 @@ public class TreeNode {
      ************************************************************************/
     public static void testTraversal(String[] args) {
         BST<Integer, Integer> bst = new BST<>();
-        _010_BST b = new _010_BST();
+        Binary_Search_Tree b = new Binary_Search_Tree();
         for (Integer ints : new Integer[] { 6, 4, 8, 3, 5 }) {
             b.put(ints);
             bst.put(ints, ints);
@@ -339,33 +338,33 @@ public class TreeNode {
 
         System.out.println("levelorder:");
         System.out.println(bst.levelOrder());
-        System.out.println(TreeNode.levelOrder(b.root));
+        System.out.println(Binary_Tree.levelOrder(b.root));
 
         System.out.println();
 
         System.out.println("pre, in, post order by recursion:");
-        System.out.println(TreeNode.dfs(b.root, 1));
-        System.out.println(TreeNode.dfs(b.root, 2));
-        System.out.println(TreeNode.dfs(b.root, 3));
+        System.out.println(Binary_Tree.dfs(b.root, 1));
+        System.out.println(Binary_Tree.dfs(b.root, 2));
+        System.out.println(Binary_Tree.dfs(b.root, 3));
 
         System.out.println();
 
         System.out.println("pre, pre1, in, post order by iter:");
-        System.out.println(TreeNode.preorderIter(b.root));
-        System.out.println(TreeNode.preorderIter1(b.root));
-        System.out.println(TreeNode.preorderSub(b.root));
-        System.out.println(TreeNode.inorderIter(b.root));
-        System.out.println(TreeNode.postorderIter(b.root));
+        System.out.println(Binary_Tree.preorderIter(b.root));
+        System.out.println(Binary_Tree.preorderIter1(b.root));
+        System.out.println(Binary_Tree.preorderSub(b.root));
+        System.out.println(Binary_Tree.inorderIter(b.root));
+        System.out.println(Binary_Tree.postorderIter(b.root));
     }
 
     public static void testBuiltTree(String[] args) {
-        TreeNode root = TreeNode.buildTree(new Integer[] { null, 2, 3 });
-        System.out.println(TreeNode.dfs(root, 1));
+        Node root = Binary_Tree.buildTree(new Integer[] { null, 2, 3 });
+        System.out.println(Binary_Tree.dfs(root, 1));
     }
 
     public static void testShow(String[] args) {
-        TreeNode root = TreeNode.buildTree(new Integer[] { 1, 2, 3, null, 4, 5, null, 6 });
-        TreeNode.show(root);
+        Node root = Binary_Tree.buildTree(new Integer[] { 1, 2, 3, null, 4, 5, null, 6 });
+        Binary_Tree.show(root);
     }
 
     public static void main(String[] args) {
